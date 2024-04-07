08cddc9e9fa11e80daa9075d91eabc94290be0d3
richhh7g <richhh7g@protonmail.com>
2024-04-06 22:35:23 -0300

    feat: hello world endpoint

--- internal/api/config/http_router.config.go
+++ internal/api/config/http_router.config.go
@@ -2,9 +2,11 @@ package config
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/richhh7g/mm-api-nfe/internal/api/response"
 )
 
 type HTTPRouterConfig struct {
@@ -21,6 +23,10 @@ func (c *HTTPRouterConfig) Configure() (*chi.Mux, error) {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID, middleware.RealIP, middleware.Logger, middleware.Recoverer)
+
+	r.Get("/hello-world", func(w http.ResponseWriter, _ *http.Request) {
+		response.NewSuccess("Hello World! MM", http.StatusOK).Send(w)
+	})
 
 	return r, nil
 }
