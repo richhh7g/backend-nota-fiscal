fd175d3c46730b81b0842ae110e485ab2c0edaa8
richhh7g <richhh7g@protonmail.com>
2024-04-07 20:30:46 -0300

    chore: add invoice datasource

---
+++ internal/infra/data/invoice/invoice.pg.datasource.go
@@ -0,0 +1,47 @@
+package invoice
+
+import (
+	"context"
+	"time"
+
+	"github.com/richhh7g/mm-api-nfe/internal/entity"
+	"github.com/richhh7g/mm-api-nfe/internal/infra/data/client/database/sqlc_pg"
+)
+
+var _ InvoiceDataSource = &InvoiceDataSourceImpl{}
+
+type InvoiceDataSource interface {
+	CreateInvoice(invoice *entity.Invoice) (*entity.Invoice, error)
+}
+
+type InvoiceDataSourceImpl struct {
+	ctx        *context.Context
+	repository *sqlc_pg.Queries
+}
+
+func NewInvoice(ctx *context.Context, client sqlc_pg.DBTX) *InvoiceDataSourceImpl {
+	return &InvoiceDataSourceImpl{
+		ctx:        ctx,
+		repository: sqlc_pg.New(client),
+	}
+}
+
+func (d *InvoiceDataSourceImpl) CreateInvoice(invoice *entity.Invoice) (*entity.Invoice, error) {
+	issuedAt, _ := time.Parse(time.RFC3339, invoice.IssuedAt)
+	receivedAt, _ := time.Parse(time.RFC3339, invoice.ReceivedAt)
+
+	params := sqlc_pg.CreateInvoiceParams{
+		ID:         invoice.ID.String(),
+		Cnpj:       invoice.CNPJ,
+		Chave:      invoice.Key,
+		EmissaoEm:  issuedAt,
+		RecebidoEm: receivedAt,
+	}
+
+	_, err := d.repository.CreateInvoice(*d.ctx, params)
+	if err != nil {
+		return &entity.Invoice{}, err
+	}
+
+	return invoice, nil
+}
